85bfea66a1596270531e53c29c5fd13a842ccef5
darkwizard242 <am900820@gmail.com>
2020-11-23 11:18:17 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE_BINARY = '/usr/local/bin/aws-vault'
+
+
 def test_awsvault_binary_exists(host):
-    assert host.file('/usr/local/bin/aws-vault').exists
+    """
+    Tests if aws-vault binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_awsvault_binary_file(host):
-    assert host.file('/usr/local/bin/aws-vault').is_file
+    """
+    Tests if aws-vault binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_awsvault_binary_which(host):
-    assert host.check_output('which aws-vault') == '/usr/local/bin/aws-vault'
+    """
+    Tests the output to confirm aws-vault's binary location.
+    """
+    assert host.check_output('which aws-vault') == PACKAGE_BINARY
